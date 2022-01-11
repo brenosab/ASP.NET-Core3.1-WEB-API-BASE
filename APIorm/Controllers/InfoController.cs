8afb95f0eaf84f9def1eec688762c91b2abd2d8f
Breno <112519@pmenos.com.br>
2022-01-10 21:54:09 -0300

    add infoController

---
+++ APIorm/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace APIorm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Application = Environment.GetEnvironmentVariable("APPLICATION"),
+                Version = Environment.GetEnvironmentVariable("VERSION"),
+                Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+            });
+        }
+    }
+}
